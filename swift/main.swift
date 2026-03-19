3db84f388e571e7524a696b88be9e7d700fa0c47
relyks7 <skylerhuahuayeye@gmail.com>
2026-03-18 20:45:25 -0400

    Actually gpu sucks at this so nevermind

--- swift/main.swift
+++
@@ -1,6 +0,0 @@
-import Metal
-import Foundation
-import Darwin
-let context=MetalContext(kernelsDirectory: URL(fileURLWithPath: "../kernels"))
-let device=context.device
-let stream = ComputeStream(context: context)
