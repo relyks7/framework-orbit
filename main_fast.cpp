e4a735dfd2951c258521f5084ed3d75d30147292
relyks7 <skylerhuahuayeye@gmail.com>
2026-08-15 22:39:13 -0400

    -Testing arm

--- main_fast.cpp
+++ main_fast.cpp
@@ -56,6 +56,7 @@ struct edge{
 class lupus{
     public:
         int n, d;
+        int per; //mirror increase amount
         vector<unordered_map<int, edge>> adj;
         vector<float> h; //nxdx1
         vector<float> force; //nxdx1
@@ -70,15 +71,25 @@ class lupus{
         void reset(){
             adj.assign(n, unordered_map<int, edge>{});
             adj[1][3]=edge{randvec(d*d, 1.0f/sqrtf(d)), vector<float>(d, 0.0f), vector<float>(d, 0.0f)};
-            adj[3][5]=edge{randvec(d*d, 1.0f/sqrtf(d)), vector<float>(d, 0.0f), vector<float>(d, 0.0f)};
-            adj[5][7]=edge{randvec(d*d, 1.0f/sqrtf(d)), vector<float>(d, 0.0f), vector<float>(d, 0.0f)};
-            adj[7][9]=edge{randvec(d*d, 1.0f/sqrtf(d)), vector<float>(d, 0.0f), vector<float>(d, 0.0f)};
-            adj[9][0]=edge{randvec(d*d, 1.0f/sqrtf(d)), vector<float>(d, 0.0f), vector<float>(d, 0.0f)};
-            adj[1][4]=adj[1][3];
-            adj[4][6]=adj[3][5];
-            adj[6][8]=adj[5][7];
-            adj[8][10]=adj[7][9];
-            adj[10][2]=adj[9][0];
+            adj[3][4]=edge{randvec(d*d, 1.0f/sqrtf(d)), vector<float>(d, 0.0f), vector<float>(d, 0.0f)};
+            adj[4][5]=edge{randvec(d*d, 1.0f/sqrtf(d)), vector<float>(d, 0.0f), vector<float>(d, 0.0f)};
+            adj[5][6]=edge{randvec(d*d, 1.0f/sqrtf(d)), vector<float>(d, 0.0f), vector<float>(d, 0.0f)};
+            adj[6][7]=edge{randvec(d*d, 1.0f/sqrtf(d)), vector<float>(d, 0.0f), vector<float>(d, 0.0f)};
+            adj[7][0]=edge{randvec(d*d, 1.0f/sqrtf(d)), vector<float>(d, 0.0f), vector<float>(d, 0.0f)};
+            per=5;
+            vector<unordered_map<int, edge>> oadj=adj;
+            for (int i=0;i<n;i++) {
+                for (auto [chl, e]:oadj[i]){
+                    int j; int k;
+                    if (i==0) j=2;
+                    else if (i==1) j=1;
+                    else j=i+per;
+                    if (chl==0) k=2;
+                    else if (chl==1) k=1;
+                    else k=chl+per;
+                    adj[j][k]=e;
+                }
+            }
             h.assign(n*d, 0.0f); h=randvec(n*d, 1.0f/sqrtf(d));
             force.assign(n*d, 0.0f);
             received_signal.assign(d, 0.0f);
@@ -114,16 +125,18 @@ class lupus{
                     matvec_transpose(w, scaled_chl_err_p, par_change, d, d, 0, 0);
                     for (int j=0;j<d;j++) force[par*d+j]-=par_change[j];
                     delta_rule(w, h, chl_err_p, received_signal, slow_learn, tanh_mag, d, d, par, 0);
-                    if (par==1 && i==3) adj[1][4].w=w;
-                    if (par==3 && i==5) adj[4][6].w=w;
-                    if (par==5 && i==7) adj[6][8].w=w;
-                    if (par==7 && i==9) adj[8][10].w=w;
-                    if (par==9 && i==0) adj[10][2].w=w;
-                    if (par==1 && i==4) adj[1][3].w=w;
-                    if (par==4 && i==6) adj[3][5].w=w;
-                    if (par==6 && i==8) adj[5][7].w=w;
-                    if (par==8 && i==10) adj[7][9].w=w;
-                    if (par==10 && i==2) adj[9][0].w=w;
+                    int j, k;
+                    if (par==1) j=1;
+                    else if (par==2) j=0;
+                    else if (par==0) j=2;
+                    else if (par<3+per) j=par+per;
+                    else j=par-per;
+                    if (i==1) k=1;
+                    else if (i==2) k=0;
+                    else if (i==0) k=2;
+                    else if (i<3+per) k=i+per;
+                    else k=i-per;
+                    adj[j][k].w=w;
                 }
             }
             for (int i=0;i<n;i++){
@@ -141,7 +154,7 @@ class lupus{
 };
 vector<pair<float, float>> gettrial(int len, unsigned int seed){
     mt19937 target_rng(seed);
-    uniform_real_distribution<float> dist_target(-1.6f, 1.6f);
+    uniform_real_distribution<float> dist_target(-1.2f, 1.2f);
     vector<pair<float, float>> ret{};
     float tx=0.0f, ty=0.0f;
     while (ret.size()<len){
@@ -152,7 +165,38 @@ vector<pair<float, float>> gettrial(int len, unsigned int seed){
     }
     return ret;
 }
-vector<pair<float,float>> trial=gettrial(30, 1225);
+vector<pair<float,float>> trial={
+    {0.771345f,-0.919253f},
+    {-0.208378f,-1.181769f},
+    {-1.039230f,-0.600000f},
+    {-1.127631f,0.410424f},
+    {-0.410424f,1.127631f},
+    {0.600000f,1.039230f},
+    {1.181769f,0.208378f},
+    {0.919253f,-0.771345f},
+    {0.000000f,-1.200000f},
+    {-0.919253f,-0.771345f},
+    {-1.181769f,0.208378f},
+    {-0.600000f,1.039230f},
+    {0.410424f,1.127631f},
+    {1.127631f,0.410424f},
+    {1.039230f,-0.600000f},
+    {0.208378f,-1.181769f},
+    {-0.771345f,-0.919253f},
+    {-1.200000f,0.000000f},
+    {-0.771345f,0.919253f},
+    {0.208378f,1.181769f},
+    {1.039230f,0.600000f},
+    {1.127631f,-0.410424f},
+    {0.410424f,-1.127631f},
+    {-0.600000f,-1.039230f},
+    {-1.181769f,-0.208378f},
+    {-0.919253f,0.771345f},
+    {0.000000f,1.200000f},
+    {0.919253f,0.771345f},
+    {1.181769f,-0.208378f},
+    {0.600000f,-1.039230f}
+};
 //vector<float> angles={0.0f,0.25f,0.5f,0.5277778f,0.5555556f,0.5833333f,0.6111111f,0.625f,0.6388889f,0.6527778f,0.6666667f,0.6805556f,0.6944444f,0.7083333f,0.7222222f,0.7361111f,0.75f};
 int total=1000; int succeeded=0;
 vector<int> endsat(trial.size()+1,0);
@@ -161,34 +205,35 @@ vector<lupus> sexti{};
 float l1=1.0f, l2=1.0f;
 float pival=3.141592653589793;
 int main(){
+    for (auto [xx, yy]:trial) cout<<xx<<' '<<yy<<'\n';
     for (int _=0;_<total;_++){
-        lupus sextus(11, 4, 0.01f, 5.0f, 1.0f, 8.0f);
+        lupus sextus(13, 4, 0.01f, 5.0f, 1.0f, 8.0f);
         sexti.push_back(sextus);
-        // float q1=-0.6f, q2=1.2f;
-        // float cx=l1*cosf(q1)+l2*cosf(q1+q2);
-        // float cy=l1*sinf(q1)+l2*sinf(q1+q2);
-        float cx=0.0f, cy=0.0f;
+        float q1=-0.6f, q2=1.2f;
+        float cx=l1*cosf(q1)+l2*cosf(q1+q2);
+        float cy=l1*sinf(q1)+l2*sinf(q1+q2);
+        //float cx=0.0f, cy=0.0f;
         bool done=true;
         for (int i=0;i<trial.size();i++){
             auto [goalx, goaly]=trial[i];
             int timer=0;
             bool converged=false;
             for (int j=0;j<100000;j++){
-                float theta=pival*1.0f*tanhf(cx);
+                // float theta=pival*1.0f*tanhf(cx);
                 //float theta=angles[i/10]*pival;
                 vector<float> sense(sextus.d,0.0f);
-                sense[0]=cx; sense[1]=cy; 
+                sense[0]=q1; sense[1]=q2; sense[2]=cx; sense[3]=cy; 
                 //sense[2]=cosf(theta); sense[3]=sinf(theta);
                 vector<float> want(sextus.d,0.0f);
-                want[0]=goalx; want[1]=goaly;
+                want[0]=q1; want[1]=q2; want[2]=goalx; want[3]=goaly;
                 vector<float> mv=sextus.step(sense, want);
-                // q1+=mv[0]; q2+=mv[1];
-                // q1=remainderf(q1, 2.0f*pival);
-                // q2=remainderf(q2, 2.0f*pival);
-                // cx=l1*cosf(q1)+l2*cosf(q1+q2);
-                // cy=l1*sinf(q1)+l2*sinf(q1+q2);
-                cx+=mv[0]*cosf(theta)-mv[1]*sinf(theta);
-                cy+=mv[0]*sinf(theta)+mv[1]*cosf(theta);
+                q1+=mv[2]; q2+=mv[3];
+                q1=remainderf(q1, 2.0f*pival);
+                q2=remainderf(q2, 2.0f*pival);
+                cx=l1*cosf(q1)+l2*cosf(q1+q2);
+                cy=l1*sinf(q1)+l2*sinf(q1+q2);
+                // cx+=mv[0]*cosf(theta)-mv[1]*sinf(theta);
+                // cy+=mv[0]*sinf(theta)+mv[1]*cosf(theta);
                 // cx+=mv[0];
                 if (abs(cx-goalx)<0.01f && abs(cy-goaly)<0.01f) timer++;
                 else timer=0;
@@ -211,6 +256,7 @@ int main(){
     }
     for (int i=0;i<=trial.size();i++) cout<<i<<": "<<endsat[i]<<'\n';
     cout<<succeeded<<'/'<<total<<'\n'<<setprecision(4)<<100.0f*succeeded/total<<"%\n";
+    cout<<"weird trial: "<<trial[2].first<<' '<<trial[2].second<<'\n';
     return 0;
 }
 /*
